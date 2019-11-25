279378755081d001e7b686f7e426367c1f01f243
Jorge Barcos <jorgebarcos88@gmail.com>
2019-11-25 10:26:00 -0300

    RoboFriend App

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+	{
+		id: 1,
+		name: 'Renzo Barcos',
+		username: 'Ren',
+		email: 'Renzito88@gmail.com'
+	},
+	{
+		id: 2,
+		name: 'Julieta Diaz',
+		username: 'Juli',
+		email: 'juli@gmail.com'
+	},
+	{
+		id: 3,
+		name: 'Jorge Barcos',
+		username: 'Jor',
+		email: 'jorge@gmail.com'
+	},
+	{
+		id: 4,
+		name: 'Leanne Graham',
+		username: 'Bret',
+		email: 'Sincere@april.biz'
+	},
+	{
+		id: 5,
+		name: 'Ervin Howell',
+		username: 'Antonette',
+		email: 'Shanna@melissa.tv'
+	},
+	{
+		id: 6,
+		name: 'Clementine Bauch',
+		username: 'Samantha',
+		email: 'Nathan@yesenia.net'
+	},
+	{
+		id: 7,
+		name: 'Patricia Lebsack',
+		username: 'Karianne',
+		email: 'Julianne.OConner@kory.org'
+	},
+	{
+		id: 8,
+		name: 'Chelsey Dietrich',
+		username: 'Lanren',
+		email: 'Lucio_Hettinger@annie.ca'
+	},
+	{
+		id: 9,
+		name: 'Mrs. Dennis Schulist',
+		username: 'Leopoldo_Corkery',
+		email: 'Karley_Dach@jasper.info'
+	},
+	{
+		id: 10,
+		name: 'Kurtis Weissnat',
+		username: 'Elwyn.Skiles',
+		email: 'Telly.Hoeger@billy.biz'
+	}
+];
